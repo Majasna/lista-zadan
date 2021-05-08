869dffcea283d00a07acb99ddd70c260068ccd4b
Lezakk <wiktor.zak94@gmail.com>
2021-05-08 20:11:58 +0200

    add list repository

---
+++ lista-zadan/src/main/java/pl/wsb/students/course/app/repository/ListRepository.java
@@ -0,0 +1,8 @@
+package pl.wsb.students.course.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.wsb.students.course.app.model.List;
+
+
+public interface ListRepository extends JpaRepository<List,Integer>{
+}
