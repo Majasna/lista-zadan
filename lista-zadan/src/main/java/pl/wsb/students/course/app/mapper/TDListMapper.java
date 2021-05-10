1142925e30c56048eb3f04b313f9cd07d020d689
Lezakk <wiktor.zak94@gmail.com>
2021-05-10 22:05:19 +0200

    add list mapper

---
+++ lista-zadan/src/main/java/pl/wsb/students/course/app/mapper/TDListMapper.java
@@ -0,0 +1,29 @@
+package pl.wsb.students.course.app.mapper;
+
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import pl.wsb.students.course.app.dto.TDListDTO;
+import pl.wsb.students.course.app.model.TDList;
+
+import java.util.List;
+
+public interface TDListMapper {
+    @Mappings({
+            @Mapping(target="id", source="entity.id"),
+            @Mapping(target="firstName", source="entity.firstName"),
+            @Mapping(target="lastName", source="entity.lastName")
+    })
+    TDListDTO tdlistToTDListDTO(TDList entity);
+
+    @Mappings({
+            @Mapping(target="id", source="dto.id"),
+            @Mapping(target="title", source="dto.title"),
+            @Mapping(target="description", source="dto.description"),
+            @Mapping(target="listType", source="dto.listType"),
+            @Mapping(target="created", source="dto.created"),
+            @Mapping(target="modified", source="dto.modified")
+    })
+    TDList TDListDTOTotdlist(TDListDTO dto);
+
+    List<TDListDTO> map(Iterable<TDList> lists);
+}
